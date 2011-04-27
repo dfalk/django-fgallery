c46c1b71c2faad501e570aab2534a9c431b7a362
dfalk <dfalk@debwork.(none)>
2011-04-27 16:37:07 +0400

    fix urls

--- urls.py
+++ urls.py
@@ -3,6 +3,6 @@ from models import Photo
 from django.views.generic.list_detail import object_list
 
 urlpatterns = patterns('',
-    url(r'^$', object_list, {'queryset': Photo.objects.all()}),
+    url(r'^$', object_list, {'queryset': Photo.objects.all()}, name='gallery'),
     url(r'^upload/$', 'fgallery.views.upload', name='gallery_upload'),
 )
